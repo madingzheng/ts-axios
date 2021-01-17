fa75dc8fa1bb36df69e53408cd328d1aca74a5d9
Mdz <747016839@qq.com>
2021-01-17 20:29:08 +0800

    处理data数据 🌚

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { AxiosRequestConfig } from './type/index'
 import xhr from './xhr'
 import { buildURL } from './helpers/url'
+import { transformRequest } from './helpers/data'
 
 function axios(config: AxiosRequestConfig): void {
   processConfig(config)
@@ -9,11 +10,16 @@ function axios(config: AxiosRequestConfig): void {
 
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformUrl(config)
+  config.data = transformData(config)
 }
 
 function transformUrl(config: AxiosRequestConfig): string {
   const { url, params } = config
   return buildURL(url, params)
+}
+
+function transformData(config: AxiosRequestConfig): any {
+  return transformRequest(config.data)
 }
 
 export default axios
