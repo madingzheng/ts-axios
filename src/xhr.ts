be4152dd890d6c3b9b0fa2690c847824a26a4fbb
Mdz <747016839@qq.com>
2021-01-17 22:05:28 +0800

    respons header and data

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,17 +1,42 @@
-import { AxiosRequestConfig } from './type'
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './type'
+import { parseHeaders } from './helpers/header'
+import { parseData } from './helpers/data'
 
-function xhr(config: AxiosRequestConfig): void {
-  const { data = null, url, method = 'GET', headers } = config
-  const request = new XMLHttpRequest()
-  request.open(method.toUpperCase(), url, true)
-  Object.keys(headers).forEach(name => {
-    if (!data && name.toLowerCase() === 'content-type') {
-      delete headers[name]
-    } else {
-      request.setRequestHeader(name, headers[name])
+function xhr(config: AxiosRequestConfig): AxiosPromise {
+  return new Promise(resolve => {
+    const { data = null, url, method = 'GET', headers, responseType } = config
+    const request = new XMLHttpRequest()
+    if (responseType) {
+      request.responseType = responseType
     }
+    request.open(method.toUpperCase(), url, true)
+    Object.keys(headers).forEach(name => {
+      if (!data && name.toLowerCase() === 'content-type') {
+        delete headers[name]
+      } else {
+        request.setRequestHeader(name, headers[name])
+      }
+    })
+    request.onreadystatechange = function handleLoad() {
+      if (request.readyState !== 4) {
+        return
+      }
+
+      const responseHeaders = request.getAllResponseHeaders()
+      const responseData =
+        responseType && responseType !== 'text' ? request.response : request.responseText
+      const response: AxiosResponse = {
+        data: parseData(responseData),
+        status: request.status,
+        statusText: request.statusText,
+        headers: parseHeaders(responseHeaders),
+        config,
+        request
+      }
+      resolve(response)
+    }
+    request.send(data)
   })
-  request.send(data)
 }
 
 export default xhr
